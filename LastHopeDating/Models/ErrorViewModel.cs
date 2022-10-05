8d117871e4411c8de051d8feb74f406193416df1
Brandon-IT <89819090+Brandon-IT@users.noreply.github.com>
2022-10-05 18:29:50 -0400

    Starting ASP.NET MVC Template
    Basic create a project file with nothing added currently

---
+++ LastHopeDating/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LastHopeDating.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
